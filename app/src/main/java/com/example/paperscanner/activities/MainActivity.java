3e46ee07949fcae6f0e831bcf59f885bc8d5d1fc
Aziz-repo <azizhassen69@gmail.com>
2021-11-19 11:48:40 +0100

    First Activity

--- app/src/main/java/com/example/paperscanner/activities/MainActivity.java
+++ app/src/main/java/com/example/paperscanner/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.paperscanner;
+package com.example.paperscanner.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.paperscanner.R;
 
 public class MainActivity extends AppCompatActivity {
 
